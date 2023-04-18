d40e2401c87d803cf60bbb2b1f63330488a928c2
Saria Elchaar <s.elchaar@hotmail.com>
2023-04-18 21:58:26 +1000

    Finalised code in Shapes.js, shapes.test.js and index.js. Prompts are functional and SVG is being created however further debugging required

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lib/shapes");
 
+// Defines a SVG Class that has a constructor with three methods for rendering
+
+class Svg{
+    constructor(){
+        this.textElement = '';
+        this.shapeElement = '';
+    }
+    render(){
+        return `<scg version= "1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+    }
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape) {
+        this.shapeElement = shape.render()
+    }
+}
 
 //Array of questions user will be prompted
 
@@ -31,11 +49,12 @@ const questions = [
 //Function to write data to file
 
 function writeToFile(fileName, data) {
-    console.log("Writing [" + data + "] to file [" + fileName + "]")
+    console.log(`Writing [${data}] to file [${fileName}]`);
 
     fs.writeFile(fileName, data, function(err) {
         if (err) {
-            return console.log(err);
+            console.log(err);
+            return;
         }
         console.log("Successful! You have generated a logo SVG!");
     });
@@ -44,61 +63,60 @@ function writeToFile(fileName, data) {
 //init function
 async function init() {
     console.log('start init');
-    var svgString = "";
-    var svg_file = "logo.svg";
+    let svgString = "";
+    let svg_file = "logo.svg";
 
     const answers = await inquirer.prompt(questions);
 
     //User Input: Text
 
-    var user_text = "";
-    if (answers.text.length > 0 && answers.text.length < 4) {
-        user_text = answers.text;
-    } else {
+    const userText = answers.text;
+    if (userText.length < 1 || userText.length > 3) {
         console.log("Invalid answer, Please enter 1-3 characters");
-
         return;
-    }
+    } 
 
-    console.log("User text: [" + user_text + "]");
+    console.log(`User text: [${userText}]`);
 
     //User input: Font color
     user_font_color = answers["text-color"];
     console.log("User font color: [" + user_font_color + "]");
 
     //User Input: Shape Color
-    user_shape_color = answers.shape;
-    console.log("User shape color: [" + user_shape_color + "]");
+    const userFontColor = answers["text-color"];
+    console.log(`User font color: [${userFontColor}]`);
 
     //User Input: Shape type
-    user_shape_type = answers["shape-type"];
-    console.log("User entered shape type = [" + user_shape_type + "]");
-
+    const userShapeColor = answers["shape-color"];
+    console.log(`User shape color: [${userShapeColor}]`);
     //User shape inputs- this is to make sure the user selects the designated shape listed and is given a result/display of what has been chosen
+   
+    const userShapeType = answers["shape-type"].toLowerCase();
+    console.log(`User entered shape type = [${userShapeType}]`);
 
-    let user_shape;
-    if (user_shape_type === "Square" || user_shape_type === "square") {
-        user_shape = new Square();
+    let userShape;
+     if (userShapeType === "square") {
+        userShape = new Square();
         console.log("User selected Square shape");
+    } else if (userShapeType === "circle") {
+        userShape = new Circle();
+        console.log("User selected Circle Shape");
+    } else if (userShapeType === "triangle") {
+        userShape = new Triangle();
+        console.log("User selected Triangle Shape");
+    } else {
+        console.log("Invalid shape. Please select from the listed shapes.");
+        return;
+    
     }
-    else if (user_shape_type === "Circle" || user_shape_type === "circle") {
-        user_shape = new Circle();
-        console.log("User selected Circle Shape")
-    }
-    else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
-        user_shape = new Triangle();
-        console.log("User selected Triangle Shape")
-    }
-    else {
-        console.log("Invalid shape, please select from the listed shapes");
-    }
-    user_shape.setColor(user_shape_color);
+  
+  userShape.setColor(userShapeColor);
 
     // Create a new SVG instance and add the shape and text elements
 
-    var svg = new Svg();
-    svg.setTextElement(user_text, user_font_color);
-    svg.setShapeElement(user_shape);
+    const svg = new Svg();
+    svg.setTextElement(userText, userFontColor);
+    svg.setShapeElement(userShape);
     svgString = svg.render();
 
     //Prints the shape to log
@@ -107,6 +125,7 @@ async function init() {
     console.log("Logo Generator Complete!");
     console.log("Writing shape to file...");
     writeToFile(svg_file, svgString);
+
 }
 init()
 
